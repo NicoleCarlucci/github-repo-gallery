f2850c843dba12623d0c2f5d0b1c7202b5374cfb
nicole_c <nicole.carlucci92@gmail.com>
2021-07-01 19:05:39 +0200

    Fetch repo data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //profile information div
 const profileInfo = document.querySelector(".overview");
 const username = "NicoleCarlucci";
+//Select the Repos List
+const repoList = document.querySelector(".repo-list");
 
 //Fetch API JSON Data
 const getInfo = async function () {
@@ -25,4 +27,23 @@ const showUserData = function(data) {
                           <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
                        </div>`;
   profileInfo.append(userInfo);
+};
+
+//Fetch Your Repos
+const getRepos = async function() {
+  const showRequest = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const response = await showRequest.json();
+  console.log(response);
+  showRepos(response);
+};
+getRepos();
+
+//Display Info About Your Repos
+const showRepos = function(repos) {
+  for (repo of repos) {
+    const listItem = document.createElement("li");
+    listItem.classList.add("repo");
+    listItem.innerHTML = `<h3>${repo.name}`;
+    repoList.append(listItem)
+  }
 };
